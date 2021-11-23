dffa03f6cdaba6a464b157743f4fc112a101d617
heqingjiang <1058073703@qq.com>
2021-11-23 10:47:31 +0800

    无重复字符的最长子串

--- src/main/java/com/superhakce/Main.java
+++ src/main/java/com/superhakce/Main.java
@@ -1,14 +1,12 @@
 package com.superhakce;
 
-import com.superhakce.leetcode.FindTwoSum;
+import com.superhakce.leetcode.LongestNoRepeatSubstring;
 
 public class Main {
 
     public static void main(String[] args) {
-        FindTwoSum findTwoSum = new FindTwoSum();
-        int[] a = {2, 5, 5, 11};
-        int[] r = findTwoSum.twoSum(a, 10);
-        System.out.println(r[0] + " : " + r[1]);
+        LongestNoRepeatSubstring longestNoRepeatSubstring = new LongestNoRepeatSubstring();
+        System.out.println(longestNoRepeatSubstring.lengthOfLongestSubstring("av"));
     }
 
 }
