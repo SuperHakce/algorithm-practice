81d464d715fddbeaed361d4f9ed5b40b0fbdaf3a
heqingjiang <1058073703@qq.com>
2021-12-30 14:27:39 +0800

    正则表达式

--- src/main/java/com/superhakce/Main.java
+++ src/main/java/com/superhakce/Main.java
@@ -1,12 +1,12 @@
 package com.superhakce;
 
-import com.superhakce.leetcode.LongestNoRepeatSubstring;
+import com.superhakce.leetcode.IsMatch;
 
 public class Main {
 
     public static void main(String[] args) {
-        LongestNoRepeatSubstring longestNoRepeatSubstring = new LongestNoRepeatSubstring();
-        System.out.println(longestNoRepeatSubstring.lengthOfLongestSubstring("av"));
+        IsMatch isMatch = new IsMatch();
+        System.out.println(isMatch.isMatch("aac", "aaa*c"));
     }
 
 }
